1dae799d40928dab11b6c17f5cef2a15bbbb8d52
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-04-09 00:42:52 -0500

    consolidated portfolio assets

--- js/text_to_speech.js
+++ js/text_to_speech.js
@@ -12,3 +12,13 @@ class TextToSpeech {
     this.synthesizer.cancel();
   }
 }
+
+/* This JavaScript file defines the TextToSpeech 
+class that uses the built-in Web Speech API to 
+convert text to speech. The speak() method takes 
+a string input and synthesizes it into speech, 
+which is played through the user's device 
+speakers. The stop() method cancels any currently
+playing speech. This class can be used by the 
+index.js file to allow the AI therapist to speak 
+its response messages to the patient. */
