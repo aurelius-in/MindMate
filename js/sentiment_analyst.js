2a8ee2c7d2573845ec691b5d20716ec9129411c2
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-04-09 00:39:23 -0500

    consolidated portfolio assets

--- js/sentiment_analyst.js
+++ js/sentiment_analyst.js
@@ -8,3 +8,4 @@ class SentimentAnalyst {
     return result.score;
   }
 }
+/* This JavaScript file defines the SentimentAnalyst class that uses the Sentiment library to analyze the sentiment of a given input string. The analyze() method takes a string input and returns a score between -1 and 1, where -1 indicates a negative sentiment and 1 indicates a positive sentiment. This score can be used by the index.js file to determine an appropriate facial expression and response for the AI therapist. */
